f060b1ed3f75431f98ad53c5bfa45291387dc459
Niraj-Node <nirajaghera999@gmail.com>
2024-10-23 16:42:28 +0530

    Add: Registration via OTP & SignInAdmin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const User = require("../models/user.model.js");
+const OTP = require("../models/otp.model");
+const { generateOTP, sendOTPEmail } = require("../utils/otp");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { errorHandler } = require("../utils/error.js");
@@ -21,6 +23,43 @@ const signIn = async (req, res, next) => {
 
     const isBlocked = validUser.isBlocked;
     if(isBlocked) return next(errorHandler(401, "User is blocked!"));
+    // Check if the password is correct
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
+    // Generate JWT token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    // Set the token in a cookie and respond with user data
+    res
+      .cookie("access_token", token, { 
+        sameSite: 'None',
+        secure: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Sign-In Controller
+const signInAdmin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not Found!"));
+
+    const isBlocked = validUser.isBlocked;
+    if(isBlocked) return next(errorHandler(401, "User is blocked!"));
+
+    const isAdmin = validUser.isAdmin;
+    if(isAdmin) return next(errorHandler(401, "User is not an Admin!"));
+
     // Check if the password is correct
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
@@ -97,7 +136,6 @@ const getCurrentUser = async (req, res, next) => {
   }
 };
 
-
 // SignOut Controller
 const signOut = async (req, res, next) => {
   try {
@@ -116,4 +154,54 @@ const signOut = async (req, res, next) => {
   }
 };
 
-module.exports = { test, signIn, getCurrentUser, signOut };
+// Send OTP Controller
+const sendOTP = async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (user) return next(errorHandler(400, "User already exists!"));
+
+    // Check if an OTP request already exists and remove it
+    await OTP.findOneAndDelete({ email });
+
+    const otp = generateOTP();
+    const otpExpiresAt = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes
+
+    const otpVerification = new OTP({
+      email,
+      otp,
+      otpExpiresAt,
+    });
+
+    await otpVerification.save();
+    await sendOTPEmail(email, otp);
+
+    res.status(200).json({ message: "OTP sent successfully to your email" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Verify OTP Controller
+const verifyOTP = async (req, res, next) => {
+  const { email, otp } = req.body;
+
+  try {
+    const otpRecord = await OTP.findOne({ email });
+    if (!otpRecord) {
+      return next(errorHandler(404, "OTP verification record not found!"));
+    }
+
+    if (otpRecord.otp !== otp || otpRecord.otpExpiresAt < Date.now()) {
+      return next(errorHandler(400, "Invalid or expired OTP"));
+    }
+
+    await createUser(req, res, next);
+    await OTP.findByIdAndDelete(otpRecord._id);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { test, signIn, signInAdmin, getCurrentUser, signOut, sendOTP, verifyOTP };
